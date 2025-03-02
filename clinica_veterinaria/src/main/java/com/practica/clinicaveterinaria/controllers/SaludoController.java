0cbae6cf84c37ecd327580633dcae6ac7ce899fe
facundozambellini5 <facundozambellini5@gmail.com>
2025-03-02 03:24:15 -0300

    mensaje saludo

--- clinica_veterinaria/src/main/java/com/practica/clinicaveterinaria/controllers/SaludoController.java
+++ clinica_veterinaria/src/main/java/com/practica/clinicaveterinaria/controllers/SaludoController.java
@@ -1,11 +1,13 @@
 package com.practica.clinicaveterinaria.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SaludoController {
 
-    public String Saludo(){
+    @GetMapping("/")
+    public String saludo(){
         return "VAMOS LOCO PORFIN PUDE LEVANTAR MI APLICACION LAREPUUUU";
     }
 }
